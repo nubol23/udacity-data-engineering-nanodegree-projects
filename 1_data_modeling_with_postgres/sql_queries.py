ed011c0dee7492ff16719bdb94b774483d411a27
nubol23 <rafael.rvp98@gmail.com>
2022-11-03 22:44:25 -0400

    Update level on conflic user table

--- 1_data_modeling_with_postgres/sql_queries.py
+++ 1_data_modeling_with_postgres/sql_queries.py
@@ -90,7 +90,7 @@ user_table_insert = ("""
         level
     ) VALUES (%s, %s, %s, %s, %s)
     ON CONFLICT (user_id)
-    DO NOTHING
+    DO UPDATE SET level = EXCLUDED.level
 """)
 
 song_table_insert = ("""
